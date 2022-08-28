732a20678a5f4b899a2ed890e7e0a623ad8ecf3e
lemonTree42 <76779752+lemonTree42@users.noreply.github.com>
2022-08-28 15:51:52 +0200

    Update Hotel.java

--- src/Hotel.java
+++ src/Hotel.java
@@ -2,6 +2,9 @@ public class Hotel {
     private int capacity;
     private int capacity2;
     private int capacity3;
+    private int capacity4;
+    private int capacity5;
+    private int capacity6;
 
     public Hotel(int capacity) {
         this.capacity = capacity;
